6946fb1863347f3fc2e38b4ce062f438476881af
TakesxiSximada <takesxi.sximada@gmail.com>
2015-01-15 01:24:53 +0900

    3.5を外した

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         ],
     platforms='any',
     packages=packages,
